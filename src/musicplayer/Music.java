40ce6b8ba8164a8adaf0c8c3688603b3918500bd
Farya <farya.hemati1313@gmail.com>
2025-04-18 11:47:30 +0330

    Add one list named allMusics to Music class.

--- src/musicplayer/Music.java
+++ src/musicplayer/Music.java
@@ -1,7 +1,10 @@
 package musicplayer;
 
+import java.util.ArrayList;
+
 public class Music {
     String title;
     User singer;
     int numberOfStream = 0;
+    static ArrayList<Music> allMusics = new ArrayList<>();
 }
