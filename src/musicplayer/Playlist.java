fcf7a70bb9c780fbf96f11e34b0b1fbfd88ca584
Farya <farya.hemati1313@gmail.com>
2025-04-18 12:26:58 +0330

    Add one void method named editTitle to Playlist class.

--- src/musicplayer/Playlist.java
+++ src/musicplayer/Playlist.java
@@ -2,7 +2,25 @@ package musicplayer;
 
 import java.util.ArrayList;
 
+import static musicplayer.User.allUsers;
+
 public class Playlist {
     ArrayList<Music> playlist = new ArrayList<>();
     User owner;
+
+    void editTitle(String password , Music music , String newTitle){
+        boolean toCheck = true;
+        for (User u : allUsers){
+            if (u.password.equals(password)){
+                music.title = newTitle;
+                toCheck = false;
+                break;
+            }
+        }
+        if (!(toCheck)){
+            throw new InvalidOperationException("this password is wrong.");
+        }
+    }
+
+
 }
