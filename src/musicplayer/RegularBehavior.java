138416b6c927c943176e08d5e72a3a34ecc55b26
Farya <farya.hemati1313@gmail.com>
2025-04-18 10:44:32 +0330

    update RegularBehavior class.

--- src/musicplayer/RegularBehavior.java
+++ src/musicplayer/RegularBehavior.java
@@ -1,4 +1,19 @@
 package musicplayer;
 
 public class RegularBehavior implements UserBehavior{
+
+    @Override
+    public void createPlaylist(String Title, User Owner) {
+
+    }
+
+    @Override
+    public void playMusic(Music music) {
+
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+
+    }
 }
